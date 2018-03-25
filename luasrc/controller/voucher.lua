d28e827e00db2c14ed185144c995c3c8de2866e3
Nicolas Pace <nico@libre.ws>
2018-03-25 11:16:46 -0300

    Removes print_r from particular file.

--- luasrc/controller/voucher.lua
+++ luasrc/controller/voucher.lua
@@ -4,6 +4,8 @@ local utils = require('voucher.utils')
 local config = require('voucher.config')
 local http = require('luci.http')
 
+local debug = require('voucher.debugtools')
+
 -- /usr/lib/lua/luci/controller/voucher.lua
 
 function readAll(file)
@@ -11,34 +13,6 @@ function readAll(file)
     local content = f:read("*all")
     f:close()
     return content
-end
-
-function print_r (t, name, indent)
-    local tableList = {}
-    function table_r (t, name, indent, full)
-        local id = not full and name
-        or type(name)~="number" and tostring(name) or '['..name..']'
-        local tag = indent .. id .. ' = '
-        local out = {}	-- result
-        if type(t) == "table" then
-            if tableList[t] ~= nil then table.insert(out, tag .. '{} -- ' .. tableList[t] .. ' (self reference)')
-            else
-                tableList[t]= full and (full .. '.' .. id) or id
-                if next(t) then -- Table not empty
-                    table.insert(out, tag .. '{')
-                    for key,value in pairs(t) do 
-                        table.insert(out,table_r(value,key,indent .. '|  ',tableList[t]))
-                    end 
-                    table.insert(out,indent .. '}')
-                else table.insert(out,tag .. '{}') end
-            end
-        else 
-            local val = type(t)~="number" and type(t)~="boolean" and '"'..tostring(t)..'"' or tostring(t)
-            table.insert(out, tag .. val)
-        end
-        return table.concat(out, '\n')
-    end
-    return table_r(t,name or 'Value',indent or '')
 end
 
 function index()
@@ -128,7 +102,7 @@ function action_voucher_admin()
     end
     luci.template.render("admin_portal/voucher",{
         vouchers=utils.from_csv_to_table(config.db),
-        form=print_r(luci.http.formvalue())
+        form=debug.print_r(luci.http.formvalue())
     })
 end
 
